ff59332cd4a3a10aa07331e1da8c10ea10a4b5b4
Ram Ali <ram.lugtu97@gmail.com>
2023-12-31 02:45:43 +0100

    write first test

---
+++ test/compound_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+def test_compound():
+    start_sum = 100000
+    annual_return = 0.2473
+    annual_cost = 0.002
+    time_periods = 1
+
+    expected = 124480.54000000002
+    actual = compound(start_sum, annual_return, annual_cost, time_periods)
+    assert actual == expected
