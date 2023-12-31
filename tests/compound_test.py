79bebbaf09a43fa5a29130a1ded148c1ebcb6f28
Ram Ali <ram.lugtu97@gmail.com>
2023-12-31 04:08:02 +0100

    test compounded value after 3 years

--- tests/compound_test.py
+++ tests/compound_test.py
@@ -1,7 +1,22 @@
+import pytest
+
 from src.investing.compound import compound
 
 
-def test_compound():
+@pytest.mark.parametrize(
+    "start_sum,annual_return,annual_cost,time_periods,expected",
+    [
+        (100000, 0.2473, 0.002, 1, 124480.54000000002),
+        (
+            100000,
+            0.2473,
+            0.002,
+            3,
+            193661.51078, # what the value is after 3 years or "time periods".
+        ),
+    ],
+)
+def test_compound(start_sum, annual_return, annual_cost, time_periods, expected):
     start_sum = 100000
     annual_return = 0.2473
     annual_cost = 0.002
