f375afd483784b2de31ee90f01ab21471d598e3a
Ram Ali <ram.lugtu97@gmail.com>
2023-12-31 23:04:15 +0100

    change variable name to something more intuitive

--- src/investing/compound.py
+++ src/investing/compound.py
@@ -1,5 +1,5 @@
-def compound(sum, annual_return, annual_cost, time_periods):
-    for _ in range(time_periods):
+def compound(sum, annual_return, annual_cost, years):
+    for _ in range(years):
         sum *= (1 + annual_return) * (1 - annual_cost)
 
     return sum
@@ -8,7 +8,7 @@ def compound(sum, annual_return, annual_cost, time_periods):
 if __name__ == "__main__":
     while True:
         input_str = input(
-            "Enter: start_sum  annual_return  annual_cost  time_periods\n>"
+            "Enter: start_sum  annual_return  annual_cost  years\n>"
         )
         input_list = input_str.split()
         try:
--- tests/compound_test.py
+++ tests/compound_test.py
@@ -4,7 +4,7 @@ from src.investing.compound import compound
 
 
 @pytest.mark.parametrize(
-    "start_sum,annual_return,annual_cost,time_periods,expected",
+    "start_sum,annual_return,annual_cost,years,expected",
     [
         (100000, 0.2473, 0.002, 1, 124480.54000000002),
         (
@@ -12,16 +12,16 @@ from src.investing.compound import compound
             0.2473,
             0.002,
             3,
-            193661.51078,  # what the value is after 3 years or "time periods".
+            193661.51078,  # what the value is after 3 years.
         ),
     ],
 )
-def test_compound(start_sum, annual_return, annual_cost, time_periods, expected):
+def test_compound(start_sum, annual_return, annual_cost, years, expected):
     start_sum = 100000
     annual_return = 0.2473
     annual_cost = 0.002
-    time_periods = 1
+    years = 1
 
     expected = 124480.54000000002
-    actual = compound(start_sum, annual_return, annual_cost, time_periods)
+    actual = compound(start_sum, annual_return, annual_cost, years)
     assert actual == expected
