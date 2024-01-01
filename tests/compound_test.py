210aa183e33b82b12b11b66ce8c27ad29498388a
Ram Ali <ram.lugtu97@gmail.com>
2024-01-01 21:45:27 +0100

    remove hardcoded input in parametrized test
    and correct expected value for test of 3 years of investment

--- tests/compound_test.py
+++ tests/compound_test.py
@@ -12,16 +12,10 @@ from src.investing.compound import compound
             0.2473,
             0.002,
             3,
-            193661.51078,  # what the value is after 3 years.
+            192887.6361838944,  # what the value is after 3 years.
         ),
     ],
 )
 def test_compound(start_sum, annual_return, annual_cost, years, expected):
-    start_sum = 100000
-    annual_return = 0.2473
-    annual_cost = 0.002
-    years = 1
-
-    expected = 124480.54000000002
     actual = compound(start_sum, annual_return, annual_cost, years)
     assert actual == expected
